cb1cfd98a10e6ca84038d650c75b5ddd2fe5bf86
ByCat <kubilayuzun08@gmail.com>
2020-07-12 14:24:27 +0300

    Create force_crosshair.lua

---
+++ legendware/force_crosshair.lua
@@ -0,0 +1,18 @@
+-- ByCat#7797
+-- Version 1.0.0
+
+menu.add_check_box("Force crosshair")
+
+local function force_crosshair()
+	local zoom = cmd.get_button_state(buttons.in_attack2)
+    if menu.get_bool("Force crosshair") then
+        if (zoom == true) then
+            console.set_string("weapon_debug_spread_show", "0")
+        else
+            console.set_string("weapon_debug_spread_show", "3")
+        end
+    else
+        console.set_string("weapon_debug_spread_show", "0")
+    end
+end
+client.add_callback("on_paint", force_crosshair)
