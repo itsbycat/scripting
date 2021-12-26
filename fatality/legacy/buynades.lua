ecb7ad14f8e09f875621e8d05de39e99de49772e
ByCat <31620096+itsbycat@users.noreply.github.com>
2021-12-26 14:32:30 +0300

    Update buynades.lua

--- fatality/legacy/buynades.lua
+++ fatality/legacy/buynades.lua
@@ -1,8 +1,15 @@
 local Events        = csgo.interface_handler:get_events()
 local Callbacks     = fatality.callbacks
 local engine_client = csgo.interface_handler:get_engine_client( )
+local menu          = fatality.menu
+local config        = fatality.config
+
+local menu_value = config:add_item("buy_nade", 0)
+local checkbox = menu:add_checkbox("Nades", "misc", "misc", "Buy bot", menu_value)
 
 local function OnGameEvent(Event)
+    if not menu_value:get_bool() then return end
+    
     local EventName = Event:get_name()
     
     if EventName == "round_start" then
